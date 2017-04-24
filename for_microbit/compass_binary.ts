78ab4ae2709b4b84529259fd0e22312e9badb486
David Whale <david@thinkingbinaries.com>
2017-04-24 18:58:44 +0100

    Some feedback on display

--- for_microbit/compass_binary.ts
+++ for_microbit/compass_binary.ts
@@ -1,4 +1,18 @@
 basic.forever(() => {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
     serial.writeString(String.fromCharCode(input.compassHeading()))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
 })
-
